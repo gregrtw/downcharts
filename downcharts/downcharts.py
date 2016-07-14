02c22bfa8df97eaba3804592db9b166bbe3b290e
Greg Houle <houle.greg@gmail.com>
2016-07-14 03:41:37 -0400

    will not use lxml, using selenium instead
    to handle dynamic javascript/ajax calls
    
    Signed-off-by: Greg Houle <houle.greg@gmail.com>

--- downcharts/downcharts.py
+++ downcharts/downcharts.py
@@ -1,8 +1,10 @@
 import sys
 
-from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-parser = etree.HTMLParser()
 
 
 def main():
