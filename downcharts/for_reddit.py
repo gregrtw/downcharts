b5e3b33e72f6c6ff0f39381d69e1b4a150a8a6de
Greg Houle <houle.greg@gmail.com>
2016-08-29 20:20:27 -0400

    refactor into class
    Signed-off-by: Greg Houle <houle.greg@gmail.com>

--- downcharts/for_reddit.py
+++ downcharts/for_reddit.py
@@ -1,15 +1,22 @@
 import praw
 import configparser
 
-ini_path = "./praw.ini"
-config = configparser.ConfigParser()
-config.read(ini_path)
 
-r = praw.Reddit(
-    'DOWNCHARTS',
-    user_agent=config.get('DOWNCHARTS', 'user_agent'),
-    client_id=config.get('DOWNCHARTS', 'oauth_client_id'),
-    client_secret=config.get('DOWNCHARTS', 'oauth_client_secret'),
-    username=config.get('DOWNCHARTS', 'user'),
-    password=config.get('DOWNCHARTS', 'pswd'))
+class Reddit:
+    ini_path = "./praw.ini"
 
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read(self.ini_path)
+        self.reddit = praw.Reddit(
+            'DOWNCHARTS',
+            user_agent=config.get('DOWNCHARTS', 'user_agent'),
+            client_id=config.get('DOWNCHARTS', 'oauth_client_id'),
+            client_secret=config.get('DOWNCHARTS', 'oauth_client_secret'),
+            username=config.get('DOWNCHARTS', 'user'),
+            password=config.get('DOWNCHARTS', 'pswd'))
+
+    def __str__(self):
+        return self.reddit.user.me()
+
+subreddit = reddit.subreddit('TopMusicCharts')
