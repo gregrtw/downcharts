db9cd5ca1a8ec1cd2f97ff133052afa7c8a1361e
Greg Houle <houle.greg@gmail.com>
2016-09-01 02:36:19 -0400

    Fixed Comments and Formatting (PEP8/257)
    Signed-off-by: Greg Houle <houle.greg@gmail.com>

--- downcharts/downcharts.py
+++ downcharts/downcharts.py
@@ -1,3 +1,4 @@
+"""TopMusicChart main module of the platform to acquire, prepare and deliver music charts."""
 import sys
 import configparser
 
@@ -7,15 +8,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Driver:
+class Driver(object):
+    """Wrapper to selenium webdriver.
+
+    Wraps all the required Driver functionalities required to scrape and prepare music charts.
+
+    Attributes:
+        driver_string: String representing which driver to instanciate from selenium's webdrivers.
+        driver: Webdriver instance from Selenium.
+    """
 
     def __init__(self, driver='Chrome'):
+        """Constructor to the Driver class.
+
+        Args:
+            driver: Driver type (string). Defaults to 'Chrome'.
+        """
         self.driver_string = driver
 
     def __str__(self):
+        """String representation of a Driver instance."""
         return self.driver_string + " Driver"
 
     def setup_driver(self):
+        """Driver setup function.
+
+        Used to setup the instance of a webdriver based on
+            the passed driver type to the Constructor.
+        """
         if self.driver_string == 'Chrome':
             self.driver = webdriver.Chrome()
         if self.driver_string == 'Firefox':
@@ -23,32 +43,63 @@ class Driver:
         # TODO: Add other driver types (IE, generic, etc.)
 
     def get_source(self, url):
+        """Getter for a source webpage.
+
+        Args:
+            url: Url (string) of the source we are requesting
+        """
         self.driver.get(url)
 
 
 class Website:
+    """Class of a website hosting music charts rankings.
+
+    The Website class is the handler for all functionalities related to a website and
+        implements methods to scrape and compile music charts from a Website.
+
+    Arguments:
+        config_path: Path to the configuration options of a Website. (string)
+        config: ConfigParser instance. (ConfigParser)
+        url: URL of the website requested. (string)
+        website: Website configuration id. (string)
+            Must match a configuration id under 'config_path'
+        driver: Driver instance used to request the website. (Driver)
+        results: Results of a map between the genres and their top tracks. (dict)
+        count: Number of tracks in the Website's charts. (int)
+    """
+
     config_path = './website.ini'
     config = configparser.ConfigParser()
 
-    def __init__( self, url, website_name ):
+    def __init__(self, url, website_name):
+        """Constructor for Website class.
+
+        Args:
+            url:
+            website_name:
+        """
         self.url = url
         self.website = website_name  # must match configuration website id
         self.config.read(self.config_path)
         self.driver = Driver()
-        self.driver.setup_driver()  # could add a driver_string to Driver()
-                                    # instance to modify the default Chrome driver
-        self.results = {}  # resulting categories and tracks go here.
-        self.count = 0  # number of tracks on charts in website
+        # NOTE: Could add a driver_string to Driver() instance to modify the default Chrome driver
+        self.driver.setup_driver()
+        self.results = {}
+        self.count = 0
 
-    def __str__( self ):
+    def __str__(self):
+        """String representation of a Website instance."""
         return ' : '.join([str(self.driver), self.url, '(' + str(self.count) + ') tracks'])
 
-    def get_source( self ):
+    def get_source(self):
+        """Wrapper around the Driver url Getter."""
         self.driver.get_source(self.url)
 
-    def _parse_songs_by_genre( self, chart_genre ):
-        """
-        Private helper function to compile_chart()
+    def _parse_songs_by_genre(self, chart_genre):
+        """Private helper function to compile_chart().
+
+        Args:
+            chart_genre:
         """
         genre_id = chart_genre.get_attribute(
             self.config.get(self.website, 'parse_songs_genre_id')
@@ -84,27 +135,21 @@ class Website:
             self.count += 1
         return result
 
-    def find_charts( self ):
+    def find_charts(self):
+        """Function used to find the root node of charts within a Website."""
         d = self.driver.driver
-        WebDriverWait( d, 10 ).until(
+        WebDriverWait(d, 10).until(
             EC.presence_of_element_located(
-                    (By.CLASS_NAME,
-                        self.config.get(self.website, 'find_charts_class')
-                    )
-                )
-            )
-        root = d.find_element_by_id(
-            self.config.get(self.website, 'find_charts_root_id')
-        )
+                (By.CLASS_NAME, self.config.get(self.website, 'find_charts_class'))))
+        root = d.find_element_by_id(self.config.get(self.website, 'find_charts_root_id'))
         charts_by_genre = root.find_elements_by_xpath(
             self.config.get(self.website, 'find_charts_genre_list_xpath')
         )
         return charts_by_genre
   
-    def compile_chart( self ):
-        """
-            FIXME: add exception handling
-        """
+    def compile_chart(self):
+        """Compile the music charts of this Website."""
+        #FIXME: add exception handling.
         try:
             self.get_source()
             charts = self.find_charts()
@@ -113,20 +158,22 @@ class Website:
         finally:
             self.cleanup()
 
-    def cleanup( self ):
-        """
-            FIXME: make quit handling by Driver and refactor into better named method
-        """
+    def cleanup(self):
+        """Cleanup routine to properly shutdown the Driver instance of the Website."""
+        # FIXME: make quit handling by Driver and refactor into better named method.
         self.driver.driver.quit()
 
-    def get_results( self ):
+    def get_results(self):
+        """Getter for results dict."""
         return self.results
 
-    def get_count( self ):
+    def get_count(self):
+        """Getter for track count."""
         return self.count
 
 
 def main():
+    """Main entry to test Website platform."""
     url = "http://www.djcity.com/charts/"
     djcity = Website(url, 'djcity')
     djcity.compile_chart()
@@ -136,4 +183,3 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
-
--- downcharts/for_reddit.py
+++ downcharts/for_reddit.py
@@ -1,3 +1,4 @@
+"""TopMusicCharts for Reddit module."""
 import praw
 import configparser
 import requests
@@ -6,6 +7,18 @@ from threading import Thread
 
 
 class RedditBot(object):
+    """A class for all bot interactions with the Reddit website.
+
+    Attributes:
+        ini_path: String of the path to the configuration options
+            of Python Reddit API Wrapper (PRAW).
+        end_message: String that represents the end of the message for all comments submitted
+            by a bot instance.
+        seen_comment: List of the comment id's of previously parsed comments.
+        config: Instance of ConfigParser used to parse configuration files.
+        reddit: Reddit instance from PRAW to communicate with the Reddit API & website.
+    """
+
     ini_path = "./praw.ini"
     end_message = (
         "\n\n_______\n\n"
@@ -17,6 +30,10 @@ class RedditBot(object):
     seen_comment = []
 
     def __init__(self):
+        """Constructor for RedditBot.
+
+        Sets default values and initializes the connection to the Reddit API.
+        """
         config = configparser.ConfigParser()
         config.read(self.ini_path)
         self.reddit = praw.Reddit(
@@ -28,31 +45,35 @@ class RedditBot(object):
             password=config.get('DOWNCHARTS', 'pswd'))
 
     def __str__(self):
+        """String representation of a RedditBot."""
         return self.reddit.user.me()
 
     def run(self):
+        """Runner for RedditBot instance.
+
+        Runner that executes all the tasks required for a complete and atomic action by a bot.
+        """
         pass
 
     def process_submission(self):
+        """Process a submission and take appropriate action."""
         pass
 
     def parse_comment(self, comment):
-        """
-        Parse comment to find message and extra command parameter
-        """
+        """Parse comment to find message and extra command parameter."""
         if (
-            "topmusiccharts!" in comment.body.lower()
-            or "!topmusiccharts" in comment.body.lower()
-            and comment.id not in self.seen_comment
-            and 'TopMusicCharts' != str(comment.author)
+            "topmusiccharts!" in comment.body.lower() or
+            "!topmusiccharts" in comment.body.lower() and
+            comment.id not in self.seen_comment and
+            'TopMusicCharts' != str(comment.author)
         ):
             t = Thread(target=self.run())
             t.start()
 
     def build_reply(self):
-        """
-        Build the reply to the call.
-        Print the formatted listing (Bullet/Numbered)
+        """Build the reply to the call.
+
+        Print the formatted music listing to a end user.
         """
         pass
 
@@ -62,6 +83,7 @@ class RedditBot(object):
 # # # #
 
 def main():
+    """Main entry that launches a RedditBot session."""
     print("Launched!")
     reddit = RedditBot()
     # # # # # # # # # # # #
@@ -77,7 +99,7 @@ def main():
         "https://api.pushshift.io/reddit/search?q={0}&limit={1}".format(
             "%22TopMusicCharts%22", "100"
         ),
-        headers={ 'User-Agent': 'downcharts for reddit 0.1.1 (by /u/TopMusicCharts)'}
+        headers={'User-Agent': 'downcharts for reddit 0.1.1 (by /u/TopMusicCharts)'}
     )
     json_results = request.json()
     results = json_results["data"]
@@ -85,8 +107,6 @@ def main():
         comment['_replies'] = ''
         reddit_comment = praw.objects.Comment(reddit, comment)
         reddit.parse_comment(reddit_comment)
-
-
 
 
 if __name__ == '__main__':
